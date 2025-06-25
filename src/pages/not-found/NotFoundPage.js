3f35c7c0d428873b0c6c26f231d14afe93dfeb15
andreissh <shkudov.and@gmail.com>
2025-06-25 11:01:07 +0300

    rewrite not found and server error pages

--- src/pages/not-found/NotFoundPage.js
+++ src/pages/not-found/NotFoundPage.js
@@ -1,10 +1,32 @@
+import "./not-found.css";
+import Link from "../../components/btn/Link";
 import Block from "../../framework/Block";
 
-const template = "<div></div>";
+const template = `
+  <div class="not-found-container">
+    <h1 class="not-found-header">404</h1>
+    <p class="not-found-description">Не туда попали</p>
+    <div class="not-found-goback-container">
+      {{{ HomeLink }}}
+    </div>
+  </div>
+`;
 
 export default class NotFoundPage extends Block {
   constructor(app) {
-    super("div");
+    super("div", {
+      HomeLink: new Link({
+        href: "#",
+        id: "renderChatsBtn",
+        class: "btn-secondary",
+        text: "Назад к чатам",
+        events: {
+          click: () => {
+            app.changePage("ChatsPage");
+          },
+        },
+      }),
+    });
   }
   render() {
     return this.compile(template);
--- src/pages/server-error/ServerErrorPage.js
+++ src/pages/server-error/ServerErrorPage.js
@@ -1,10 +1,32 @@
+import "./server-error.css";
+import Link from "../../components/btn/Link";
 import Block from "../../framework/Block";
 
-const template = "<div></div>";
+const template = `
+  <div class="server-error-container">
+    <h1 class="server-error-header">500</h1>
+    <p class="server-error-description">Мы уже фиксим</p>
+    <div class="server-error-goback-container">
+      {{{ HomeLink }}}
+    </div>
+  </div>
+`;
 
 export default class ServerErrorPage extends Block {
   constructor(app) {
-    super("div");
+    super("div", {
+      HomeLink: new Link({
+        href: "#",
+        id: "renderChatsBtn",
+        class: "btn-secondary",
+        text: "Назад к чатам",
+        events: {
+          click: () => {
+            app.changePage("ChatsPage");
+          },
+        },
+      }),
+    });
   }
   render() {
     return this.compile(template);
