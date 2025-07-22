0abe1c3af41c0a9ce2de2e9ae4e9c0f89f11e62d
andreissh <shkudov.and@gmail.com>
2025-07-22 22:05:32 +0300

    fix newline error

--- src/components/form/Form.ts
+++ src/components/form/Form.ts
@@ -1,21 +1,21 @@
 import Block from "@/framework/Block";
 
 type FormProps = {
-    [key: string]: unknown;
-}
+	[key: string]: unknown;
+};
 
 const template = `
     <form class="{{ class }}">
         {{{ children }}}
     </form>
-`
+`;
 
 export default class Form extends Block {
-    constructor(props: FormProps) {
-        super('div', props)
-    }
+	constructor(props: FormProps) {
+		super("div", props);
+	}
 
-    render(): HTMLElement {
+	render(): HTMLElement {
 		return this.compile(template);
 	}
 }
